3f7a8190d6969d6ef8d7bf62d93f9eb1d86a7051
MitchVerweerden <mverweer@avans.nl>
2019-10-16 19:19:30 +0200

    Enumeration toegevoegd, basis voor linked list aangemaakt

--- Goudkoorts/Goudkoorts/Track.cs
+++ Goudkoorts/Goudkoorts/Track.cs
@@ -6,5 +6,27 @@ namespace Goudkoorts
 {
     public class Track
     {
+        protected Track _North;
+        protected Track _South;
+        protected Track _East;
+        protected Track _West;
+        protected Direction _Direction;
+        
+        public Track TrackInDirection(Direction direction)
+        {
+            switch(direction)
+            {
+                case Direction.North:
+                    return _North;
+                case Direction.East:
+                    return _East;
+                case Direction.South:
+                    return _South;
+                case Direction.West:
+                    return _West;
+                default:
+                    return (Track)null;
+            }
+        }
     }
 }
